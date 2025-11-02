c2c6832b3aa7222b10f585902221619907c5debe
Suprit Kumar <suprit.22scse1012364@galgotiasuniversity.edu.in>
2025-11-03 01:36:27 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
 const App = () => {
   return (
-    <div>
+    <>
       <Sidebar/>
-    </div>
+      <Main/>
+    </>
   )
 }
 
